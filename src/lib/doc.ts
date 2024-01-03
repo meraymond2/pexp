dc235cd24d81a9651f6a7cc33d4527f698a9c8c0
Michael Raymond <michael_raymond@fastmail.com>
2024-01-03 12:28:05 +0000

    sketch out docs

--- src/lib/doc.ts
+++ src/lib/doc.ts
@@ -1,6 +1,58 @@
 export type Document = null
 
+let id = 0
+
 export type Text = {
   _tag: "text"
-
+  id: number
+  s: string
 }
+export const Text = (s: string): Text => ({
+  _tag: "text",
+  id: id++,
+  s,
+})
+
+export type NL = {
+  _tag: "new-line"
+  id: number
+}
+export const NL: NL = {
+  _tag: "new-line",
+  id: id++,
+}
+
+export type Concat = {
+  _tag: "concat"
+  id: number
+  a: Document
+  b: Document
+}
+export const Concat = (a: Document, b: Document): Concat => ({
+  _tag: "concat",
+  id: id++,
+  a,
+  b,
+})
+
+export type Align = {
+  _tag: "align"
+  id: number
+  d: Document
+}
+export const Align = (d: Document): Align => ({
+  _tag: "align",
+  id: id++,
+  d,
+})
+
+export type Flatten = {
+  _tag: "flatten"
+  id: number
+  d: Document
+}
+export const Flatten = (d: Document): Flatten => ({
+  _tag: "flatten",
+  id: id++,
+  d,
+})
